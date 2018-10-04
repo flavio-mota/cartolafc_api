49c1e5ff73964c622416475231f467fe1f223e5b
d3fineme <felipe-alves-09@hotmail.com>
2018-10-03 22:17:09 -0300

    Erros tirados

--- load-scripts/status_rodada_bd.py
+++ load-scripts/status_rodada_bd.py
@@ -13,12 +13,12 @@ try:
     data = requests.get(url).json()
     
     print("Carregando os dados dos status dos atletas na rodada - - - - - Aguarde")
-    for atleta in data['atleta']:
+    for atleta in data['atletas']:
         result = []
         
         rodada_id = atleta['rodada_id']
         atleta_id = atleta['atleta_id']
-        id_status = atleta['status']
+        id_status = atleta['status_id']
 
         result = [rodada_id, atleta_id, id_status]
         cur.execute("""INSERT into cartola_fc.tb_status_rodada
